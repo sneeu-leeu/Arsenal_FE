b47da2b120a061e0ab0090a50dbbaed27003a9b8
Stephan Annandale <sa070197@gmail.com>
2024-02-02 17:49:49 +0000

    cleaning up

--- player_api_front/src/App.js
+++ player_api_front/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import 'datatables.net';
 import './App.css';
 import PlayerList from './components/PlayerList';
